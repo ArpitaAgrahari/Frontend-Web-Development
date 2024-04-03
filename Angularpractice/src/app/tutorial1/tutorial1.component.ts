5e9379006e22d72829e57c9dbdebec0a53d1b43b
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-04-03 11:31:42 +0530

    Add analytics ID to angular.json

---
+++ Angularpractice/src/app/tutorial1/tutorial1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial1',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial1.component.html',
+  styleUrl: './tutorial1.component.css'
+})
+export class Tutorial1Component {
+
+}
